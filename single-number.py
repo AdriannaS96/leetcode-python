b403fc30a80708726047ddf9950c162f29c1e2a1
Adrianna <83674146+AdriannaS96@users.noreply.github.com>
2026-09-03 11:40:56 +0100

    Implement single number finder in single-number.py

---
+++ single-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        seen = set()
+
+        for number in nums:
+            if number in seen:
+                seen.remove(number)
+            else:
+                seen.add(number)
+
+        return seen.pop()
+                
+                
