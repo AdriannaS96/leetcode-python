0740d983017cb37d9bacc9e9dd31e79b09400f90
Adrianna <83674146+AdriannaS96@users.noreply.github.com>
2026-09-08 15:44:50 +0100

    Implement findDuplicate method to detect duplicates

---
+++ findDuplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        seen = set()
+
+        for number in nums:
+            if number in seen:
+                return number
+            seen.add(number)
+        return None
